0320a0f1634ffca5c33e88a5a03862ea706191f6
marco-ht <marco-ht@outlook.it>
2025-10-24 22:24:21 +0200

    ex03

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: mpierant <marvin@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 08:00:53 by mpierant          #+#    #+#             */
-/*   Updated: 2025/10/24 08:26:44 by mpierant         ###   ########.fr       */
+/*   Updated: 2025/10/24 22:04:41 by mpierant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ Zombie* zombieHorde( int N, std::string name )
     int i;
     Zombie  *ptr;
     
-    ptr= new Zombie[N];
+    ptr = new Zombie[N];
     i = 0;
     while (i < N)
     {
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,15 +6,32 @@
 /*   By: mpierant <marvin@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 08:43:46 by mpierant          #+#    #+#             */
-/*   Updated: 2025/10/24 08:46:06 by mpierant         ###   ########.fr       */
+/*   Updated: 2025/10/24 22:21:15 by mpierant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include "Zombie.hpp"
 
 int main(void)
 {
-    
+    std::string str;
+    std::string *stringPTR;
+    std::string &stringREF = str;
+
+    stringPTR = &str;
+    str = "HI THIS IS BRAIN";
+    std::cout << &str << std::endl;
+    std::cout << stringPTR << std::endl;
+    std::cout << &stringREF << std::endl;
+    std::cout << str << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
 }
+
+/* std::string str2;
+str2 = str;
+std::cout << &str2 << std::endl;
+str2 has a different address while stringREF has the exact same address.
+If I modify str2, it doesn't affect str.
+If I modify stringREF, I'm modifying str itslef. */
