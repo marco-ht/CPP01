b8ed19e3e4e48b882b6dc07fca4745034ad54ab3
marco-ht <marco-ht@outlook.it>
2025-11-04 04:06:51 +0100

    new test

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpierant <marvin@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 23:00:08 by mpierant          #+#    #+#             */
-/*   Updated: 2025/10/24 23:47:34 by mpierant         ###   ########.fr       */
+/*   Updated: 2025/11/04 04:04:25 by mpierant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int main()
     {
         Weapon  club = Weapon("crude spiked club");
         HumanB jim("Jim");
+        jim.attack();   // test attack without weapon
         jim.setWeapon(club);
         jim.attack();
         club.setType("some other type of club");
